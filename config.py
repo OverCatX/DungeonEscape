2a67771b1410ac61c4a74472ff6eb3a9c5058ff3
Bhumipat Kusalatham <overcatpocket@gmail.com>
2025-04-05 00:55:43 +0700

    Add basic collision and trap mechanics

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+    TILE_SIZE = 32
+    FPS = 60
+    COLORS = {
+        "bg": (30, 30, 30),
+        "player": (0, 200, 255),
+        "enemy": (200, 0, 0)
+    }
